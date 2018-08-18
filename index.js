df79a3bafbaf327285d40ae8c7fa0161f0133665
Dog2puppy <colecooper2005@icloud.com>
2018-08-18 14:06:58 +0000

    Add new file

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const Koa = require('koa')
+const morgan = require('koa-morgan')
+
+const accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' })
+const app = new Koa()
+
+app.use(morgan('combined', { stream: accessLogStream }))
+
+app.use((ctx) => {
+  ctx.body = 'Hello, world!'
+})
+
+app.listen(8888)
