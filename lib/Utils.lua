8c5bd297fba03c216a7e7fc92790d74964f7670f
Dog2puppy <colecooper2005@icloud.com>
2018-08-19 16:16:45 +0000

    Update Utils.lua

--- lib/Utils.lua
+++ lib/Utils.lua
@@ -1,5 +1,9 @@
 local Utils = {}
 
+local function CalculateMinMax(startTime, rank)
+    
+end
+
 local function Utils:FindMatch(matches, info)
     if matches and info then
         local rank = info.matchmaking_level
