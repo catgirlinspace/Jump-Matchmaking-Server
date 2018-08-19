533e5626f64884c3df0b0d0ad8a99c881457b399
Dog2puppy <colecooper2005@icloud.com>
2018-08-19 09:15:46 -0400

    Added package.lua

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "Dog2puppy/Jump-Matchmaking-Server",
+    version = "0.0.1",
+    description = "Matchmaking server for Jump. Matches players based on skill and friends.",
+    tags = { "lua", "roblox", "server", "http", "luvit" },
+    license = "MIT",
+    author = { name = "Dog2puppy", email = "colecooper2005@icloud.com" },
+    homepage = "https://gitlab.com/Dog2puppy/Jump-Matchmaking-Server",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
